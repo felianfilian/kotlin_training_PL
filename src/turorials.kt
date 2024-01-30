862a6a98a12502fb3109939faf04cfbe9372a700
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-01-30 12:23:51 +0700

    array test

--- src/turorials.kt
+++ src/turorials.kt
@@ -1,13 +1,7 @@
 
 fun main() {
-    print("number: ")
-    var myInput = readln();
-    println();
-    print("output: ")
-    if(myInput != null) {
-        println(myInput.toInt() * myInput.toInt());
-    }
-
+    var myArray = arrayOf("mario", "peach");
+    println(myArray[1]);
 }
 
 
